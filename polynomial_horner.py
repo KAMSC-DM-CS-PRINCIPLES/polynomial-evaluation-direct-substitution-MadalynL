4f492b761c489be6565c03807356a28d4ea0a14a
MadalynL <madalynmlindberg@gmail.com>
2025-10-02 08:56:14 -0400

    Update polynomial_horner.py

--- polynomial_horner.py
+++ polynomial_horner.py
@@ -2,7 +2,7 @@ def evaluate_polynomial_horner(degree, x, constant_term, *coefficients):
     # TODO: Implement polynomial evaluation using Horner's method
     # TODO: Print step-by-step evaluation (S0, S1, S2, etc.)
     # TODO: Return final polynomial result
-    s = coefficients[0]
+    s = int(coefficients[0])
     k =1
     while k<degree:
         s = x*s + coefficients[k]
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     # TODO: Get user input for degree, x, constant term, and coefficients
     # TODO: Call evaluate_polynomial_horner function
     # TODO: Ask user if they want to run again
-    YN = true
+    YN = True
     while YN:
         degree = int(input("Degree: "))
         x = int(input("x: "))
